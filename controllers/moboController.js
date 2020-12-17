17604e1fd8d64cc10090573f8f917aba28c24b9c
Manoel Lopes <Manoel_Lopes@jabil.com>
2020-12-17 14:43:50 +0000

    Starting the frontend

--- controllers/moboController.js
+++ controllers/moboController.js
@@ -1,2 +1,27 @@
 const db = require('../config/db.config');
 const Motherboad = db.Motherboard;
+
+// Get all Motherboards on DB
+exports.getAllMotherboards = (req, res) => {
+  try {
+    Motherboad.findAll({
+      attributes: [
+        'testedDate',
+        'assetId',
+        'macAddress',
+        'tester',
+        'result',
+        'comments',
+      ],
+    }).then((moboInfos) => {
+      res.status(200).json({
+        Motherboards: moboInfos,
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      Message: 'Error fetching data!',
+      Error: error.message,
+    });
+  }
+};
--- routers/routes.js
+++ routers/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const databaseFiller = require('../helpers/databaseFiller');
+const moboController = require('../controllers/moboController');
 
 const path = __basedir + '/views/';
 
@@ -9,5 +10,7 @@ router.get('/', (req, res) => {
   databaseFiller.getMoboData();
   res.sendFile(path + 'index.html');
 });
+
+router.get('/api/v1.0/getallmotherboards', moboController.getAllMotherboards);
 
 module.exports = router;
