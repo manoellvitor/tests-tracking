1a56f4768735b99cd251d65c1cd269692f313d5a
Manoel Lopes <Manoel_Lopes@jabil.com>
2020-12-21 13:00:05 +0000

    helmet removed

--- resources/js/searchSerialNumber.js
+++ resources/js/searchSerialNumber.js
@@ -1,26 +1,25 @@
-window.onload = function () {
-  var a = document.getElementById('searchBtn');
-  var chart = document.getElementById('chartID');
-  var title = document.getElementById('titleID');
-  var table = document.getElementById('table');
-  var searchInput = document.getElementById('searchInput');
-  var flag = 0;
+var a = document.getElementById('searchBtn');
+var chart = document.getElementById('chartID');
+var title = document.getElementById('titleID');
+var table = document.getElementById('table');
+var searchInput = document.getElementById('searchInput');
+var flag = 0;
 
-  const xhttp = new XMLHttpRequest();
+const xhttp = new XMLHttpRequest();
 
-  xhttp.open('GET', '/api/v1.0/searchbysn/' + searchInput, false);
-  console.log(searchInput.value);
-  xhttp.send();
+xhttp.open('GET', '/api/v1.0/searchbysn/' + searchInput, false);
+console.log(searchInput.value);
+xhttp.send();
 
-  a.onclick = function () {
-    if (flag == 0) {
-      chart.remove();
-      title.innerHTML = 'Search';
+a.onclick = function () {
+  if (flag == 0) {
+    chart.remove();
+    title.innerHTML = 'Search';
 
-      const searchData = JSON.parse(xhttp.responseText);
+    const searchData = JSON.parse(xhttp.responseText);
 
-      searchData.Motherboards.forEach((mobo) => {
-        const x = `
+    searchData.Motherboards.forEach((mobo) => {
+      const x = `
       <tr>
         <td>${mobo.testedDate}</td>
         <td>${mobo.assetId}</td>
@@ -31,15 +30,14 @@ window.onload = function () {
       </tr>
       
     `;
-        document.getElementById('table').removeAttribute('hidden');
-        document.getElementById('tbody').innerHTML =
-          document.getElementById('tbody').innerHTML + x;
-      });
+      document.getElementById('table').removeAttribute('hidden');
+      document.getElementById('tbody').innerHTML =
+        document.getElementById('tbody').innerHTML + x;
+    });
 
-      flag++;
-      return false;
-    } else {
-      table.remove();
-    }
-  };
+    flag++;
+    return false;
+  } else {
+    table.remove();
+  }
 };
