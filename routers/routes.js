1176d4278bff6bd1e1471835f9c83020ce2c76a0
Manoel Lopes <Manoel_Lopes@jabil.com>
2020-12-22 13:25:32 +0000

    Finished Table with dinamic data from the API

--- routers/routes.js
+++ routers/routes.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 const db = require('../config/db.config');
 const databaseFiller = require('../helpers/databaseFiller');
 const moboController = require('../controllers/moboController');
-const searchController = require('../controllers/searchController');
 
 router.get('/', (req, res) => {
   db.sequelize.sync({ force: true }).then(() => {
     databaseFiller.getMoboData();
-    res.render('index');
+    res.render('index', { title: 'Welcome' });
   });
 });
 
-router.get('/motherboards', (req, res) => {
-  res.sendFile(path + 'motherboards.html');
+router.get('/mobos', async (req, res) => {
+  try {
+    await axios
+      .get('http://localhost:5000/api/v1.0/getallmotherboards')
+      .then((mobosAPI) => {
+        res.render('mobo', {
+          title: 'Motherboards',
+          mobos: mobosAPI.data.Motherboards,
+        });
+      });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 router.get('/api/v1.0/getallmotherboards', moboController.getAllMotherboards);
-router.get('/api/v1.0/searchbysn/:sn', searchController.searchBySerialNumber);
 
 module.exports = router;
