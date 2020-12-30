b9719624abc5c587a929678d3f594ea47da6004c
Manoel Lopes <manoelvitorka@gmail.com>
2020-12-30 11:52:11 +0000

    Adding link to my Linkedin

--- resources/js/exportDataToExcel.js
+++ resources/js/exportDataToExcel.js
@@ -5,7 +5,7 @@ function exportTableToExcel(tableID, filename = '') {
   var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
 
   // Specify file name
-  filename = filename ? filename + '.xls' : 'excel_data.xls';
+  filename = filename ? filename + '.xls' : 'hardware_tested.xls';
 
   // Create download link element
   downloadLink = document.createElement('a');
