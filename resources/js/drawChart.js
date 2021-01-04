bf31076ea1cb5e973cdc92652954a78382698e1e
Manoel Lopes <manoelvitorka@gmail.com>
2021-01-04 14:13:25 +0000

    Working on tge charts

--- resources/js/drawChart.js
+++ resources/js/drawChart.js
@@ -6,29 +6,46 @@ function getK2ts() {
   return axios.get('/api/v1.0/getallk2ts');
 }
 
+function getK2cs() {
+  return axios.get('/api/v1.0/getallk2cs');
+}
+
+function getK2xs() {
+  return axios.get('/api/v1.0/getallk2xs');
+}
+
 function getDimms() {
   return axios.get('/api/v1.0/getalldimms');
 }
 
-Promise.all([getMobos(), getDimms(), getK2ts()]).then(function (results) {
-  const mobos = results[0];
-  const dimms = results[1];
-  const k2ts = results[2];
+Promise.all([getMobos(), getDimms(), getK2ts(), getK2cs(), getK2xs()]).then(
+  function (results) {
+    const mobos = results[0];
+    const dimms = results[1];
+    const k2ts = results[2];
+    const k2cs = results[3];
+    const k2xs = results[4];
+    const annas =
+      Object.keys(k2ts.data.K2ts).length +
+      Object.keys(k2cs.data.K2cs).length +
+      Object.keys(k2xs.data.K2xs).length;
 
-  drawChart(
-    Object.keys(mobos.data.Motherboards).length,
-    Object.keys(dimms.data.Dimms).length,
-    Object.keys(k2ts.data.K2ts).length,
-  );
-  drawChartBar(mobos.data.Motherboards, dimms.data.Dimms);
-});
+    drawChart(
+      Object.keys(mobos.data.Motherboards).length,
+      Object.keys(dimms.data.Dimms).length,
+      annas,
+    );
+    drawChartBar(
+      mobos.data.Motherboards,
+      dimms.data.Dimms,
+      k2ts.data.K2ts,
+      k2cs.data.K2cs,
+      k2xs.data.K2xs,
+    );
+  },
+);
 
-// axios.get('/api/v1.0/getallmotherboards').then((resp) => {
-//   drawChart(Object.keys(resp.data.Motherboards).length);
-//   drawChartBar(resp.data.Motherboards);
-// });
-
-function drawChart(mobos, dimms, k2ts) {
+function drawChart(mobos, dimms, annas) {
   var ctx = document.getElementById('totalHardwareChart').getContext('2d');
   var totalHardwareChart = new Chart(ctx, {
     type: 'pie',
@@ -37,7 +54,7 @@ function drawChart(mobos, dimms, k2ts) {
       datasets: [
         {
           label: 'Hardware Tested',
-          data: [mobos, k2ts, dimms, 55, 221],
+          data: [mobos, annas, dimms, 200, 300],
           backgroundColor: [
             'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
@@ -66,11 +83,20 @@ function drawChart(mobos, dimms, k2ts) {
   });
 }
 
-function drawChartBar(mobos, dimms) {
+function drawChartBar(mobos, dimms, k2ts, k2cs, k2xs) {
   let moboFail = 0;
   let moboPass = 0;
   let dimmFail = 0;
   let dimmPass = 0;
+  let k2tFail = 0;
+  let k2tPass = 0;
+  let k2cfail = 0;
+  let k2cPass = 0;
+  let k2xfail = 0;
+  let k2xPass = 0;
+
+  let annasFail = 0;
+  let annasPass = 0;
 
   mobos.forEach((mobo) => {
     if (
@@ -96,6 +122,45 @@ function drawChartBar(mobos, dimms) {
     }
   });
 
+  k2ts.forEach((k2t) => {
+    if (
+      k2t.result == 'Fail' ||
+      k2t.result == 'Faild' ||
+      k2t.result == 'Fail Test'
+    ) {
+      k2tFail++;
+    } else if (k2t.result == 'Pass') {
+      k2tPass++;
+    }
+  });
+
+  k2cs.forEach((k2c) => {
+    if (
+      k2c.result == 'Fail' ||
+      k2c.result == 'Faild' ||
+      k2c.result == 'Fail Test'
+    ) {
+      k2cfail++;
+    } else if (k2c.result == 'Pass') {
+      k2cPass++;
+    }
+  });
+
+  k2xs.forEach((k2x) => {
+    if (
+      k2x.result == 'Fail' ||
+      k2x.result == 'Faild' ||
+      k2x.result == 'Fail Test'
+    ) {
+      k2xfail++;
+    } else if (k2x.result == 'Pass') {
+      k2xPass++;
+    }
+  });
+
+  annasFail = k2tFail + k2cFail + k2xFail;
+  annasPass = k2tPass + k2cPass + k2xPass;
+
   var ctx = document.getElementById('FailAndPass').getContext('2d');
   var FailAndPass = new Chart(ctx, {
     type: 'bar',
@@ -119,7 +184,7 @@ function drawChartBar(mobos, dimms) {
             'rgba(0, 0, 0, 255)',
           ],
           borderWidth: 1,
-          data: [moboFail, 3, dimmFail, 10, 20],
+          data: [moboFail, annasFail, dimmFail, 10, 20],
         },
         {
           label: 'Pass',
@@ -138,7 +203,7 @@ function drawChartBar(mobos, dimms) {
             'rgba(0, 0, 0, 255)',
           ],
           borderWidth: 1,
-          data: [moboPass, 2, dimmPass, 45, 54],
+          data: [moboPass, annasPass, dimmPass, 45, 54],
         },
       ],
     },
