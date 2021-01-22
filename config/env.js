82ec2db16890f9821f9a2e2dcaf47917f4970dad
Manoel Lopes <manoelvitorka@gmail.com>
2021-01-22 10:59:39 +0000

    Working on date range

--- config/env.js
+++ config/env.js
@@ -17,10 +17,20 @@ const env = {
 
 module.exports = env;
 
+// Production Paths
 // filesPath: {
 //   moboPath: 'G:/Publics/Common/Debug/Testing/Foxconn motherboards/Tested/',
 //   dimmPath: 'G:/Publics/Common/Debug/Testing/DIMMs/Tested/',
 //   k2tPath: 'G:/Publics/Common/Debug/Testing/K2T/Tested/',
 //   k2cPath: 'G:/Publics/Common/Debug/Testing/K2C/Tested/',
 //   k2xPath: 'G:/Publics/Common/Debug/Testing/K2X/Tested/',
+// },
+
+// DEV Paths
+// filesPath: {
+//   moboPath: 'C:/Users/2514164.JABIL/Desktop/TestedHardware/MB/',
+//   dimmPath: 'C:/Users/2514164.JABIL/Desktop/TestedHardware/DIMM/',
+//   k2tPath: 'C:/Users/2514164.JABIL/Desktop/TestedHardware/K2T/',
+//   k2cPath: 'C:/Users/2514164.JABIL/Desktop/TestedHardware/K2C/',
+//   k2xPath: 'C:/Users/2514164.JABIL/Desktop/TestedHardware/K2X/',
 // },
--- controllers/moboController.js
+++ controllers/moboController.js
@@ -1,6 +1,8 @@
 const db = require('../config/db.config');
 const moment = require('moment');
 const Motherboad = db.Motherboard;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 // Get all Motherboards on DB
 exports.getAllMotherboards = (req, res) => {
@@ -19,6 +21,39 @@ exports.getAllMotherboards = (req, res) => {
       res.status(200).json({
         Motherboards: moboInfos,
         moment: moment,
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      Message: 'Error fetching data!',
+      Error: error.message,
+    });
+  }
+};
+
+exports.getMoboByDate = (req, res) => {
+  const start = req.body.start;
+  const end = req.body.end;
+  try {
+    console.log(start, end);
+    Motherboad.findAll({
+      order: [['testedDate', 'DESC']],
+      attributes: [
+        'testedDate',
+        'assetId',
+        'macAddress',
+        'tester',
+        'result',
+        'comments',
+      ],
+      where: {
+        testedDate: {
+          [Op.between]: [start, end],
+        },
+      },
+    }).then((moboInfos) => {
+      res.status(200).json({
+        Motherboards: moboInfos,
       });
     });
   } catch (error) {
