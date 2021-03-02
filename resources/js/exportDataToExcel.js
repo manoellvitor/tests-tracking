25151520000847443c9063d43e152c7d211e3b7f
Manoel Lopes <manoelvitorka@gmail.com>
2021-03-02 07:34:21 +0000

    Adding new script to save table to excel

--- resources/js/exportDataToExcel.js
+++ resources/js/exportDataToExcel.js
@@ -1,30 +1,41 @@
-function exportTableToExcel(tableID, filename = '') {
-  var downloadLink;
-  var dataType = 'application/vnd.ms-excel';
-  var tableSelect = document.getElementById(tableID);
-  var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+// function exportTableToExcel(tableID, filename = '') {
+//   var downloadLink;
+//   var dataType = 'application/vnd.ms-excel';
+//   var tableSelect = document.getElementById(tableID);
+//   var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
 
-  // Specify file name
-  filename = filename ? filename + '.xls' : 'hardware_tested.xls';
+//   // Specify file name
+//   filename = filename ? filename + '.xls' : 'hardware_tested.xls';
 
-  // Create download link element
-  downloadLink = document.createElement('a');
+//   // Create download link element
+//   downloadLink = document.createElement('a');
 
-  document.body.appendChild(downloadLink);
+//   document.body.appendChild(downloadLink);
 
-  if (navigator.msSaveOrOpenBlob) {
-    var blob = new Blob(['\ufeff', tableHTML], {
-      type: dataType,
-    });
-    navigator.msSaveOrOpenBlob(blob, filename);
-  } else {
-    // Create a link to the file
-    downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+//   if (navigator.msSaveOrOpenBlob) {
+//     var blob = new Blob(['\ufeff', tableHTML], {
+//       type: dataType,
+//     });
+//     navigator.msSaveOrOpenBlob(blob, filename);
+//   } else {
+//     // Create a link to the file
+//     downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
 
-    // Setting the file name
-    downloadLink.download = filename;
+//     // Setting the file name
+//     downloadLink.download = filename;
 
-    //triggering the function
-    downloadLink.click();
-  }
+//     //triggering the function
+//     downloadLink.click();
+//   }
+// }
+
+function exportTableToExcel() {
+  let table = document.getElementsByTagName('table'); // you can use document.getElementById('tableId') as well by providing id to the table tag
+  TableToExcel.convert(table[0], {
+    // html code may contain multiple tables so here we are refering to 1st table tag
+    name: `exportedTable.xlsx`, // fileName you could use any name
+    sheet: {
+      name: 'Sheet 1', // sheetName
+    },
+  });
 }
